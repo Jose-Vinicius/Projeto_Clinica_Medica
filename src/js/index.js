51e61bda18c9150ca93050aa3c18b270cee16ae1
Jose Vinicius <Jose-Vinicius@users.noreply.github.com>
2023-10-26 19:33:10 -0300

    feat: added comments into code

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
+// Variáveis aonde é armazenado os parâmetros vindos da URL
 const urlParametros = new URLSearchParams(window.location.search);
 const parametros = Object.fromEntries(urlParametros.entries());
 
+//Variaveis para "Pegar as os elementos html"
 let nomePaciente = document.getElementById("ficha-nome");
 let cpfPaciente = document.getElementById("ficha-cpf");
 let telefonePaciente = document.getElementById("ficha-telefone");
@@ -16,10 +18,12 @@ let especialidades = document.getElementById("ficha-especialista");
 let dataConsulta = document.getElementById("ficha-dataConsulta");
 let horariosDisponiveis = document.getElementById("ficha-horariosConsulta");
 
+//Função responsável por adicionar um zero para datas aonde somente teria 1 digito ex: 5 -> 05
 function inserir2digitosData(data){
     return data.toString().padStart(2, '0')
 }
 
+//Função responsável por formatar a data vinda do formulário HTML
 function transformarData(dataFormatar){
     let data = new Date(dataFormatar);
     let ano = data.getFullYear();
@@ -30,6 +34,7 @@ function transformarData(dataFormatar){
 }
 
 
+//Inserir as os dados em tela, apos estarem formatados
 nomePaciente.innerHTML = parametros.nomePaciente;
 cpfPaciente.innerHTML = parametros.cpfPaciente;
 telefonePaciente.innerHTML = parametros.telefonePaciente;
